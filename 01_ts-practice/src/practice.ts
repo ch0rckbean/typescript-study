6846120c63eb5c4b0862b55c8256dc7a21c879fc
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-02-23 16:06:11 +0900

    study: function

--- 01_ts-practice/src/practice.ts
+++ 01_ts-practice/src/practice.ts
@@ -1,3 +1,4 @@
+// -- 타입 예시
 const msg: string = 'In StarBucks';
 console.log(msg);
 
@@ -7,3 +8,12 @@ let nullableNumber: number | null = null;
 let color: 'red' | 'orange' | 'yellow' = 'red';
 color = 'yellow';
 // color='green';  //에러남
+
+// -- 함수작성
+function sumArr(nums: number[]): number {
+  return nums.reduce((acc, cur) => acc + cur, 0);
+}
+sumArr([1, 2, 3, 4]);
+function returnNothing(): void {
+  console.log('HI');
+}
