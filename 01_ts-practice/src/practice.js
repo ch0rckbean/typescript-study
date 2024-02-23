11a2c11ae54de93f080eacd7f5b751589ada4eb9
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-02-23 16:02:18 +0900

    study: basic type

---
+++ 01_ts-practice/src/practice.js
@@ -0,0 +1,4 @@
+'use strict';
+const msg = 'In StarBucks';
+console.log(msg);
+// 컴파일 과정에서 명시 타입이 사라짐
---
+++ 01_ts-practice/src/practice.ts
@@ -0,0 +1,9 @@
+const msg: string = 'In StarBucks';
+console.log(msg);
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'orange' | 'yellow' = 'red';
+color = 'yellow';
+// color='green';  //에러남
